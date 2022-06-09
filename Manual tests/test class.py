b41b2e11cdec49320f5c99dcf214e34c3b96d2dc
Mark Melotto <M.Melotto@student.tudelft.nl>
2022-06-09 19:50:22 +0200

    took in all the work I had previously done. It contains the haarcascade that is the library for faces, facial req for the functions that detect faces, a testing space, the MAIN and pictures

---
+++ Manual tests/test class.py
@@ -0,0 +1,29 @@
+from Picture.picturetaker import return_a_picture
+from Facial_Recoqnition.picture_facial_reqoc import facial_reqocnition
+from Facial_Recoqnition.video_facial_recoq import video_FR
+
+class doorbell:
+
+    def __init__(self):
+        self.img = return_a_picture()
+
+    def someone_at_the_door(self):
+        '''in order to take a new picture when someone is at the door:'''
+        self.img = return_a_picture()
+
+    def picture_FR(self):
+        img = self.img
+        facial_reqocnition(img)
+
+    def continuous_stream(self):
+        video_FR()
+
+if __name__=="__main__":
+
+    DB = doorbell()
+
+    DB.picture_FR()
+    DB.someone_at_the_door()
+    print('some shit')
+    # DB.picture_FR()
+    # DB.continuous_stream()
