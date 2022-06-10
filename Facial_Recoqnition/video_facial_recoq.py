4543b9f65c32ea2f5133a267d661299ffc3b2201
Mark Melotto <M.Melotto@student.tudelft.nl>
2022-06-10 12:15:55 +0200

    added a fun function to the video FR where it just tracks a face :)

--- Facial_Recoqnition/video_facial_recoq.py
+++ Facial_Recoqnition/video_facial_recoq.py
@@ -28,5 +28,34 @@ def video_FR():
     # Release the VideoCapture object
     cap.release()
 
+def follow_a_face():
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_fontalface.xml')
+
+    # To capture video from webcam.
+    cap = cv2.VideoCapture(0)
+    # To use a video file as input
+    # cap = cv2.VideoCapture('filename.mp4')
+
+    while True:
+        # Read the frame
+        _, img = cap.read()
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            face = img[y:y+w, x:x+h]
+        # Display
+        cv2.imshow('img', face)
+        # Stop if escape key is pressed
+        k = cv2.waitKey(30) & 0xff
+        if k==27:
+            break
+    # Release the VideoCapture object
+    cap.release()
+
 if __name__ == "__main__":
-    video_FR()
+    # video_FR()
+    follow_a_face()
