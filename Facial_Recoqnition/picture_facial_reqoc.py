184c79626ae0a3c68ee45fb82f351ae8c6dec07b
Mark Melotto <M.Melotto@student.tudelft.nl>
2022-06-10 12:12:33 +0200

    added a function to get a face from a picture

--- Facial_Recoqnition/picture_facial_reqoc.py
+++ Facial_Recoqnition/picture_facial_reqoc.py
@@ -3,6 +3,7 @@ https://towardsdatascience.com/face-detection-in-2-minutes-using-opencv-python-9
 '''
 
 import cv2
+import numpy as np
 
 def facial_recognition(img):
     # Load the cascade
@@ -20,6 +21,27 @@ def facial_recognition(img):
     cv2.imshow('img', img)
     cv2.waitKey()
 
+def get_face_from_picure(img):
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_fontalface.xml')
+    # Read the input image
+    # img = cv2.imread('test_image.jpg')
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # face = img.copy()
+
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        # face = np.array((w, h, 3))
+        face = img[y:y+w, x:x+h]
+        # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
+    # Display the output
+    cv2.imshow('img', face)
+    cv2.waitKey()
+
 if __name__ == "__main__":
     img = cv2.imread('test_image.jpg')
-    facial_recognition(img)
+    # facial_recognition(img)
+    get_face_from_picure(img)
