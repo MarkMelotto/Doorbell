df625b3a2dbd42888dcdf3a18c08cb9ef5b6f23e
Mark Melotto <M.Melotto@student.tudelft.nl>
2022-06-10 12:21:46 +0200

    made a function that saves a face from an input image, must only be 1 face tho, also added a test version to use the test image

--- Facial_Recoqnition/picture_facial_reqoc.py
+++ Facial_Recoqnition/picture_facial_reqoc.py
@@ -21,7 +21,7 @@ def facial_recognition(img):
     cv2.imshow('img', img)
     cv2.waitKey()
 
-def get_face_from_picure(img):
+def show_face_from_picure(img):
     # Load the cascade
     face_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_fontalface.xml')
     # Read the input image
@@ -41,7 +41,47 @@ def get_face_from_picure(img):
     cv2.imshow('img', face)
     cv2.waitKey()
 
+def save_face_from_picure(img):
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_fontalface.xml')
+    # Read the input image
+    # img = cv2.imread('test_image.jpg')
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # face = img.copy()
+
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        # face = np.array((w, h, 3))
+        face = img[y:y+w, x:x+h]
+        # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
+    # Display the output
+    cv2.imwrite("../Pictures/faces/current_face.jpg",face) #save image
+
+def test_save_face_from_picure(img):
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_fontalface.xml')
+    # Read the input image
+    # img = cv2.imread('test_image.jpg')
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # face = img.copy()
+
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        # face = np.array((w, h, 3))
+        face = img[y:y+w, x:x+h]
+        # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), 2)
+    # Display the output
+    cv2.imwrite("../Pictures/faces/test_1.jpg",face) #save image
+
+
 if __name__ == "__main__":
     img = cv2.imread('test_image.jpg')
     # facial_recognition(img)
-    get_face_from_picure(img)
+    # show_face_from_picure(img)
+    test_save_face_from_picure(img)
